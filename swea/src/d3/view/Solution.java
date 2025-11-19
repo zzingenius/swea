db09f41fc0cbfccd19dc321ccd6b8dcf04279b0f
zzing <wkdwldms2001@naver.com>
2025-11-19 18:34:53 +0900

    1206. 건물 조망권 6/10

---
+++ swea/src/d3/view/Solution.java
@@ -0,0 +1,57 @@
+package d3.view;
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+
+		int T = 10; //테스트 케이스 총 개수
+		Scanner sc = new Scanner(System.in);
+		int size; //배열 개수
+		int focus; // 기준이 되는 아파트 층수
+		
+		for(int test_case = 1; test_case <= T; test_case++ ) {
+			long view = 0; //총 확보된 조망권
+			int right = 0;
+			int left = 0;
+			
+			size = sc.nextInt();
+			
+			int[] building = new int[size];
+			for(int i= 0 ; i < size; i++) {
+				building[i] = sc.nextInt(); //배열에 값 집어넣기
+			}
+			
+			for(int i = 2 ; i < size-3 ; i++) {
+				if(building[i-1]>building[i-2]) {
+					left= minus(building[i], building[i-1]);
+				}else {
+					//building[1-2]가 더 큰 경우
+					left= minus(building[i], building[i-2]);
+				}
+				
+				if(building[i+1]> building[i+2]) {
+					right = minus(building[i], building[i+1]);
+				}else {
+					right = minus(building[i], building[i+2]);
+				}
+				
+				if(left > right) {
+					view += right;
+				}else {
+					view +=left;
+				}
+			}
+			System.out.println("#" + test_case + " " + view);
+		}
+		
+	}
+	private static int minus(int a, int b) {
+		int result;
+		if(a > b ) {
+			result = a -b;
+		}else {
+			result = 0;
+		}
+		return result;
+	}
+
+}
